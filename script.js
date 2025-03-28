60a4f8e983b989dc51f1fcd69bc8194135b4f69a
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2025-03-29 00:48:08 +0530

    Added modal open close handling

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+function onShowModalClick() {
+  document.querySelector('[data-testid="modal-dialog"]').showModal();
+}
+
+function onCloseModalClick() {
+  document.querySelector('[data-testid="modal-dialog"]').close();
+}
+
+document
+  .querySelector('[data-testid="show-modal-btn-1"]')
+  .addEventListener("click", onShowModalClick);
+document
+  .querySelector('[data-testid="show-modal-btn-2"]')
+  .addEventListener("click", onShowModalClick);
+document
+  .querySelector('[data-testid="show-modal-btn-3"]')
+  .addEventListener("click", onShowModalClick);
+document
+  .querySelector('[data-testid="modal-close-btn"]')
+  .addEventListener("click", onCloseModalClick);
